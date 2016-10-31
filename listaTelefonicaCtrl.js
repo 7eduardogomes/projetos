6bb3a97eff8cdf26a77abb5992b6c279dedad260
7eduardogomes <eduarduhgomes10@gmail.com>
2016-10-30 23:20:02 -0200

    Add files via upload

---
+++ listaTelefonicaCtrl.js
@@ -0,0 +1,26 @@
+﻿angular.module("listaTelefonica").controller("listaTelefonicaCtrl", function ($scope) {
+    $scope.app = "Lista Telefonica";
+    $scope.contatos = [
+        { nome: "Eduardo Gomes", telefone: "98075-5870", operadora: { nome: "Vivo", codigo: 15 } },
+        { nome: "Mayara Baptista", telefone: "99219-5870", operadora: { nome: "Vivo", codigo: 15 } }
+    ];
+    $scope.operadoras = [
+        { nome: "Oi", codigo: 14 },
+        { nome: "Vivo", codigo: 15 },
+        { nome: "Tim", codigo: 41 }
+    ];
+    $scope.adicionarContato = function (contato) {
+        $scope.contatos.push(angular.copy(contato));
+        delete $scope.contato;
+        $scope.contatoForm.$setPristine();
+    };
+    $scope.apagarContatos = function (contatos) {
+        $scope.contatos = contatos.filter(function (contato) {
+            if (!contato.selecionado) return contato;
+        });
+    };
+    $scope.ordenarPor = function (campo) {
+        $scope.criterioDeOrdenacao = campo;
+        $scope.direcaoDaOrdenacao = !$scope.direcaoDaOrdenacao;
+    };
+});
